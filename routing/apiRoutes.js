9c2e3bae062284cbec58cdb0105cacd671a89264
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2018-12-05 18:55:26 -0800

    fixed logic to determine best matched gamer-friend

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -10,6 +10,33 @@ module.exports = function(app) {
 
     // POST route to /api/friends (This is used to handle incoming survey results and compatability logic)
     app.post("/api/friends", function(req, res) {
-        friendsData.push(req.body);
+
+        var newFriendScore = req.body.scores;
+
+        var lastDiff = 41;
+        var friendNum = 0;
+
+        for (var i = 0; i < friendsData.length; i++) {
+
+            var totalDiff = 0;
+
+            for (var j = 0; j < newFriendScore.length; j++) {
+                if (newFriendScore[j] > friendsData[i].scores[j]) {
+                    var diffScore = newFriendScore[j] - friendsData[i].scores[j];
+                } else if (newFriendScore[j] < friendsData[i].scores[j]) {
+                    var diffScore = friendsData[i].scores[j] - newFriendScore[j];
+                }
+                totalDiff += diffScore;
+            }
+            if (totalDiff < lastDiff) {
+                lastDiff = totalDiff;
+                friendNum = i;
+            }
+        }
+
+        console.log("You're best matched gamer-friend is: " + friendsData[friendNum].name);
+        console.log("This is a link to their photo: " + friendsData[friendNum].photo)
+        console.log("The total difference is: " + lastDiff);
+        
     });
 };
