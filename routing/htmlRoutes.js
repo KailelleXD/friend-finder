d987ebe7ebc952ee9ceaf37dd419907d4c0ab1bc
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2018-12-05 22:08:03 -0800

    added a / route to htmlRoutes.js

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -10,6 +10,11 @@ module.exports = function(app) {
     });
 
     // Default GET route, catch-all that leads to home.html
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../app/public/home.html"));
+    });
+
+    // Default GET route, catch-all that leads to home.html
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../app/public/home.html"));
     });
